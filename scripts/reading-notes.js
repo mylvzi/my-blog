d2275a93431bfc0806f5de14e7afb5418b2ffb90
mylvzi <2314394028@qq.com>
2026-06-12 10:26:24 +0800

    Update site 2026/06/12 周五 10:26:23.82

--- scripts/reading-notes.js
+++ scripts/reading-notes.js
@@ -1,6 +1,5 @@
-'use strict';
 
-const { renderHeatmap } = require('./heatmap-helper');
+'use strict';
 
 // Mark reading note posts so Stellar's index.ejs skips them (line 25: post.indexing != false)
 hexo.extend.filter.register('before_post_render', function(data) {
@@ -44,13 +43,6 @@ function renderReadingIndex(months) {
     html += '</li>';
   });
   html += '</ul>';
-
-  // Collect all posts for heatmap
-  const allPosts = [];
-  months.forEach(function(m) {
-    allPosts.push.apply(allPosts, m.posts);
-  });
-  html += renderHeatmap(allPosts, hexo.config.root + 'reading/');
 
   return html;
 }
